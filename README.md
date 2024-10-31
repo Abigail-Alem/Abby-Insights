7888043b4e0ac5dbfbe9c92c8c23d1ab82f1fb90
Abigail <abigailbirhanalem@gmail.com>
2024-10-30 23:05:59 -0400

    Develop personal portfolio

---
+++ README.md
@@ -0,0 +1,7 @@
+# Personal Website
+
+This personal website is a dynamic web application developed using React, a leading JavaScript library for crafting interactive user interfaces. It serves as a portfolio that highlights my skills, experiences, and projects, providing a comprehensive view of my work. The design prioritizes responsiveness and visual appeal, ensuring a seamless user experience across a range of devices.
+
+Explore the live version of this project hosted on GitHub Pages.
+
+Thank you :)
---
+++ src/content_option.js
@@ -0,0 +1,188 @@
+const logotext = "ABBY";
+const meta = {
+    title: "Abigail Birhan Alem",
+    description: "",
+};
+
+const introdata = {
+    title: "I’m Abigail Birhan Alem",
+    animated: {
+        first: "Culturally Conscious STEM Enthusiast.",
+        second: "Dedicated Community Advocate & Environmentalist.",
+        third: "Adaptable Learner & Global Citizen.",
+    },
+    description: "A driven and adaptable student with a diverse background spanning leadership, community service, and emerging skills in various fields. With experience in environmental advocacy, cultural exchange, and foundational coding, I am committed to building skills that can create a meaningful impact.",
+    about: "Explore my journey across continents, my dedication to community and learning, and how I aim to combine technical expertise with a passion for social change. Here, you'll find insights into my academic achievements, extracurricular projects, and aspirations to use technology for a better world.",
+    your_img_url: "https://i.imgur.com/xVHtn7R.jpg",
+};
+
+const dataabout = {
+    title: "A bit about my self",
+    aboutme: "I am Abigail Alem, a dedicated high school senior with a passion for learning and community engagement. My diverse background includes leadership roles, where I advocate for environmental awareness and sustainability. My experiences have instilled in me a strong commitment to social equity and a desire to help those in need. Growing up in Ethiopia and moving to the United States has shaped my perspective, fostering a deep appreciation for cultural diversity and resilience. I enjoy hiking, reading, and exploring new ideas, all while focusing on my academic pursuits and extracurricular activities. I aim to leverage my abilities to create positive change in the world around me.",
+};
+const extracurricular = [{
+    title: "Youth Farmers",
+    description: "President",
+},{
+    title: "IT",
+    description: "Basic Programming Mentor",
+},{
+    title: "American Corner, US Embassy, AA, Ethiopia",
+    description: "Volunteer Mentor (Leadership)",
+},{
+    title: "Mini-Media Club",
+    description: "Event Organizer",
+},{
+    title: "Anti-Drug and Addiction",
+    description: "Secretary",
+},{
+    title: "Environmental Club",
+    description: "Secretary",
+},{
+    title: "Charity Club",
+    description: "Fundraiser, Member",
+},{
+    title: "Math Club",
+    description: "Member",
+},{
+    title: "Know Your Country Club",
+    description: "Member",
+},
+];
+
+const education = [{
+    title: "High School - East High School | EHS",
+    description: "Sep 2024 - Present | GPA - 4.0/4.0 | Coursework: College Prep Math, Financial Literacy, Ceramics, English Literature, US History, PE, AVID, Communications",
+},
+{
+    title: "Middle School & High School- EthioParents’ School | EPS",
+    description: "GPA - 4.0/4.0 | Coursework: Physics, Biology, Math, Chemistry, English, Amharic, History, Geography, IT, PE, Civics, Agriculture | Charity Club | Math Club | Know your country Club | Mini media Club",
+},
+{
+    title: "Basic Coding Skills - IT Education",
+    description: "Aug 2022 | Starting in my sophomore year (grade 10), I received mentorship from an IT professional on Saturdays outside of school hours, developing foundational Python programming skills through hands-on projects. As I progressed, I took on a mentorship role, helping newcomers understand coding principles, troubleshoot code, and cultivate a collaborative learning environment.",
+},{
+    title: "Entrepreneurship, Leadership & Communication - Young African Leaders Initiative (YALI) | Certificate",
+    description: "June 2023 - August 2023 | Start-up project at Axiom Financials - consulted on the go-to-market strategy | International project at Valeo - did extensive research on the future of mobility after Covid-19",
+},
+];
+const worktimeline = [
+];
+
+const skills = [{
+        name: "Problem Solving",
+        value: 98,
+    },
+    {
+        name: "Leadership",
+        value: 99,
+    },
+    {
+        name: "Consultation",
+        value: 90,
+    },
+    {
+        name: "Project Management",
+        value: 96,
+    },
+    {
+        name: "Programming - Java, Python",
+        value: 89,
+    },
+    {
+        name: "Team Management",
+        value: 98,
+    },
+];
+
+const services = [{
+        title: "Leadership & Community Engagement",
+        description: "Team Management: Successfully led and managed club initiatives, from environmental projects to charity events, ensuring alignment with goals and meaningful impact. | Public Speaking: Communicated effectively in diverse settings, from school clubs to community awareness events, building confidence in presenting ideas. | Mentorship: Guided peers and younger students in coding and leadership, creating an inclusive learning environment.",
+    },
+    {
+        title: "Technical Proficiency",
+        description: "Programming Languages (Python, Java): Strong foundational skills in Python from sophomore mentorship sessions; adept in Java for school and personal projects. | Problem Solving: Applied analytical skills to troubleshoot code, tackle project challenges, and mentor peers effectively.",
+    },
+    {
+        title: "Environmental Advocacy",
+        description: "Sustainability Awareness: Led initiatives on the impact of industrialization and the environmental benefits of paper over plastic bags, promoting sustainable practices. | Project Execution: Collaborated on detailed visual and hands-on projects that conveyed environmental issues, bridging science with community awareness.",
+    },
+    {
+        title: "Communication & Interpersonal Skills",
+        description: "Empathy: Developed deep empathy through personal experiences, including pandemic hardships and cultural transitions, which informed leadership and community service efforts. | Cross-Cultural Understanding: Bridged experiences across U.S. and Ethiopian cultures, gaining adaptability and an inclusive approach to teamwork and learning. | Language Proficiency: Bilingual in English and Amharic, with a foundational knowledge of Spanish, enhancing communication across cultural settings.",
+    },
+    {
+        title: "Adaptability & Resilience",
+        description: "Self-Initiated Learning: Proactively engaged in online courses and personal development during the pandemic, building independence and a growth mindset. | Flexibility: Adapted to new academic systems and social environments in the U.S. education system, overcoming challenges with discipline and persistence.",
+    },
+];
+
+
+const hobbies = [{
+    img: "https://images.unsplash.com/photo-1603205431090-8dcdfcb54897?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mjh8fGxlYXJuaW5nfGVufDB8fDB8fHww&auto=format&fit=crop&w=800&q=60",
+    description: "Constantly Curious: Learning and Exploring",
+},
+{
+    img: "https://images.unsplash.com/photo-1512649406163-f231484b4d69?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1yZWxhdGVkfDE0fHx8ZW58MHx8fHx8&auto=format&fit=crop&w=800&q=60",
+    description: "Wordsmith at Heart: Journaling & Reading (one of my favorite authors - Danielle Steel)",
+},
+{
+    img: "https://images.unsplash.com/photo-1554907984-15263bfd63bd?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NDR8fGFydCUyMGdhbGxlcnl8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=800&q=60",
+    description: "Art Admirer: Sculpture | Embracing Creativity",
+},
+{
+    img: "https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fG5hdHVyZXxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60",
+    description: "Nature's Wanderer: Hiking and Nature Enthusiast",
+},
+
+];
+
+const dataportfolio = [
+    {
+        img: "https://plus.unsplash.com/premium_photo-1682144407366-83198874f4fb?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        description: "Collaborated on a visual project to demonstrate the impact of greenhouse gasses on climate change, designing an architectural scene with one side representing pure nature and the other an industrial city, separated by a block but connected by a river that starts clean and becomes polluted as it reaches the factory side, symbolizing the environmental effects of industrialization.",
+        // link: "",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1719754519952-213032732b90?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTJ8fHBsYXN0aWMlMjBiYWd8ZW58MHx8MHx8fDA%3D",
+        description: "Conducted research and led a public awareness initiative advocating for the use of paper bags over plastic, highlighting the environmental impact of plastic's slow decomposition and promoting sustainable alternatives to reduce long-term ecological harm.",
+        // link: "",
+    },
+    {
+        img: "https://images.unsplash.com/photo-1488590528505-98d2b5aba04b?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8ZnJvbnQlMjBlbmR8ZW58MHx8MHx8fDA%3D",
+        description: "Collaborated on a group project to develop a simple website, focusing on basic front-end design, responsive layout, and ensuring ease of navigation for users.",
+        // link: "",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "abigailbirhanalem@gmail.com",
+    YOUR_FONE: "(919)337-8263",
+    description: "A passionate student looking to build innovative solutions that make a difference by creating a positive global impact.",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com/Abigail-Alem",
+    // linkedin: "https://www.linkedin.com/in/eden-alem-7b936b1ab/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    extracurricular,
+    education,
+    hobbies,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
