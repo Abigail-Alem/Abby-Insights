e2a095552901bed6e8d8d901b65f26420fd2b8d5
Abigail-Alem <abigailbirhanalem@gmail.com>
2026-01-17 13:55:40 -0700

    Update App.js

--- src/app/App.js
+++ src/app/App.js
@@ -1,9 +1,6 @@
 import React, { useEffect } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
-import {
-  HashRouter as Router,
-  useLocation,
-} from "react-router-dom";
+import { HashRouter as Router, useLocation } from "react-router-dom";
 import withRouter from "../hooks/withRouter";
 import AppRoutes from "./routes";
 import Headermain from "../header";
@@ -17,6 +14,7 @@ function _ScrollToTop(props) {
   }, [pathname]);
   return props.children;
 }
+
 const ScrollToTop = withRouter(_ScrollToTop);
 
 export default function App() {
